0907dc08172c13a21be0b3edd9ba012bfe195c15
Алексей Кондрашов <dackfy@MacBook-Air-Aleksej.local>
2023-08-04 11:41:13 +0300

    createBoard

--- sudoku.js
+++ sudoku.js
@@ -1,27 +1,31 @@
+const { EOL } = require('os');
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  const arrOfSudoku = [];
+  const board = boardString.split('');
+  for (let i = 0; i < boardString.length; i += 9) {
+    arrOfSudoku.push(board.slice(i, i + 9));
+  }
+  console.table(arrOfSudoku);
 }
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
